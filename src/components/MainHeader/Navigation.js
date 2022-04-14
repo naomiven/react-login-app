0674f63677f3a88d4444f1fea545bdb08bd95dcb
naovqz <naomi.mira@gmail.com>
2022-04-13 22:00:02 -0700

    use useContext (more elegant) for consumer

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,38 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  // Consumer takes a child (a function)
+  // Takes in context data from auth-context as input
+  // Returns JSX code which has access to that data
+  const ctx = useContext(AuthContext);
+
   return (
-    // Consumer takes a child (a function)
-    // Takes in context data from auth-context as input
-    // Returns JSX code which has access to that data
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
