afe6cd7846175cb7a5044ad45a4827b7af77d9cf
naovqz <naomi.mira@gmail.com>
2022-04-18 21:15:01 -0700

    use React Context to pass in onLogout handler

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   // Consumer takes a child (a function)
   // Takes in context data from auth-context as input
   // Returns JSX code which has access to that data
@@ -24,7 +24,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
